e346199c8a8dfb81b160b9c86cb45694cd9282c0
gabriel barina <barinagabriel2007@yahoo.com>
2015-10-14 21:35:43 +0300

    fixed repeating occurences

--- src/php_files/create_initial_arbor.php
+++ src/php_files/create_initial_arbor.php
@@ -67,15 +67,37 @@
 					$local_heading = str_replace ("\r" , "" , str_replace ("\n" , "" , $headings[$i_key]));
 
 					if (trim ($rowNode) != ''){
-						execute_sql('<create_initial_arbor.php>', $con, "UPDATE nodes SET link_to = CONCAT_WS(',', link_to, '".$rowNode."') WHERE name = '".$local_heading."'");
+						$node = preventDuplicate ($con, trim ($rowNode), trim ($local_heading));
+						execute_sql('<create_initial_arbor.php>', $con, "UPDATE nodes SET link_to = '".$node."' WHERE name = '".$local_heading."'");
 						
 						// also add the inverse of it to have the linkTo attribute set
-						execute_sql('<create_initial_arbor.php>', $con, "UPDATE nodes SET link_to = CONCAT_WS(',', link_to, '".$local_heading."') WHERE name = '".$rowNode."'");
+						$node = preventDuplicate ($con, trim ($local_heading), trim ($rowNode));
+						execute_sql('<create_initial_arbor.php>', $con, "UPDATE nodes SET link_to = '".$node."' WHERE name = '".$rowNode."'");
 					}
 				}
 			}
 		}
 
+	}
+
+	// function to prevent duplicated nodes to being added into the link_to fields
+	function preventDuplicate ($con, $to_add, $row_node) {
+		$node_search 	= $con->query("SELECT link_to FROM nodes WHERE name = '".$row_node."' LIMIT 1");
+		$link_to 		= $node_search->fetch_assoc()['link_to'];
+		$link_to 		= explode(',', $link_to);
+
+		$add = true;
+		foreach ($link_to as $key => $value) {
+			if ($value == $to_add) {
+				$add = false;
+			}
+		}
+
+		if ($add) {
+			array_push ($link_to, $to_add);
+		}
+
+		return trim (implode (',', $link_to), ',');
 	}
 
 	// random floating-point generator
--- src/php_files/simulator.php
+++ src/php_files/simulator.php
@@ -40,12 +40,21 @@
 
 	// function to treat information related
 	function consumerPhase ($con) {
-		$nodes = execute_sql_and_return ('<simulator.php>', $con, "SELECT * FROM nodes");
-		while ($row = mysqli_fetch_assoc ($nodes)) {
+		$possibleBuyers = [];
+		$nodes 			= execute_sql_and_return ('<simulator.php>', $con, "SELECT * FROM nodes");
 
-			//print_r($row);
+		while ($row = mysqli_fetch_assoc ($nodes)) {
 			// set producer to "true"
 			$row['is_producer'] = 1;
+
+			// get a list of possible buyers
+			$buyers = execute_sql_and_return ('<simulator.php>', $con, "SELECT name, link_to FROM nodes WHERE needs_product='".$row['has_product']."'");
+			while ($buy = mysqli_fetch_assoc ($buyers)) {
+				$possibleBuyers[$buy['name']] = $buy['link_to'];
+			}
+
+			print_r($possibleBuyers);
+
 
 			$possibleBuyers = getPossibleBuyers ($con, $nodes, $row);
 
@@ -59,11 +68,20 @@
 
 	// function to return a list of possible buyers using the basic Dijkstra's algorithm
 	function getPossibleBuyers ($con, $nodes, $row) {
-		$list = '';
+		$list = [];
 		// http://codereview.stackexchange.com/questions/75641/dijkstras-algorithm-in-php
 		// http://stackoverflow.com/questions/6598791/how-to-optimize-dijkstra-code-in-php
 		// http://stackoverflow.com/questions/4867716/more-than-640-000-elements-in-the-array-memory-problem-dijkstra
 		// https://en.wikipedia.org/wiki/Dijkstra's_algorithm
+		// https://github.com/phpmasterdotcom/DataStructuresForPHPDevs/blob/master/Graphs/graph-dijkstra.php
+		// http://odino.org/the-shortest-path-problem-in-php-demystifying-dijkstra-s-algorithm/
+
+
+
+
+
+
+
 
 		return $list;
 	}
