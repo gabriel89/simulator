8a0df755d6235d42f05d312c45fce666737487de
gabriel barina <barinagabriel2007@yahoo.com>
2016-01-28 21:53:55 +0200

    removed console.log

--- src/halfviz.js
+++ src/halfviz.js
@@ -67,7 +67,6 @@
 				    async: false,
 				    success: function(arbor) {
 				    	console.log('Reading arbor-structure from DB and writing inital log');
-				    	console.log(arbor);
 				    	$('#code').val(arbor);
 				   	},
 					error: function(e) {console.log('Error reading DB structure'); if (e) console.log(e);}
