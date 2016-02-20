e946a9e884ace42193364f156d3c39a77e3f976d
gabriel barina <barinagabriel2007@yahoo.com>
2016-02-20 20:50:49 +0200

    code-adaption for consumer-phase

--- src/php_files/consumer_phase.php
+++ src/php_files/consumer_phase.php
@@ -18,11 +18,10 @@
 			$providers 		= getProviders($b);
 			
 			for ($p = 0; $p < sizeof($providers); $p++) {
-				if ($providers[$p] != null) {
+				if ($providers[$p] != null)
 					$list = array_merge ($list, array (BFS ($b, $providers[$p])));
-				} else {
+				else
 					echo "<br />No providers found for $b <br /><br />";
-				}
 			}
 			
 			$consumer_path = array_merge ($consumer_path, $list);
@@ -36,7 +35,8 @@
 	// function to finalize transaction:
 	// for each element of $consumer_path we need to sell the product from the producer (first node) to the consumer (last node)
 	// each intermediate node will add a personal profit to the total value of the previous node, thus product final price = initial price + profit node1 + profit node2 + ...
-	function finalizeTransaction ($con, $nodes, $consumer_path) {
+	function finalizeTransaction ($con, $consumer_path) {
+		global $nodes;
 		addToLog ("\n\n\n;--------------------------------\n;        FINALIZING TRANSACTIONS\n;--------------------------------\n\n");
 
 		$last_sold_out_msg 	= '';
@@ -45,34 +45,32 @@
 		foreach ($consumer_path as $current_path) {
 
 			// retrieve the names of the nodes involved in this transaction | the first node is the seller | the last node is the buyer
-			$inter_nodes = explode (',', $current_path);
-
+			$inter_nodes = explode ('->', $current_path);
 			// retrieve seller and buyer nodes from path
-			$seller_node = &$nodes[indexTo ($inter_nodes[0], $nodes)];	
-			
+			$seller_node = $nodes[array_reverse($inter_nodes)[0]];
 			//	check if the seller_node still has product to sell
 			if (check_seller_has_product ($seller_node)) {
+				$buyer_node = $nodes[$inter_nodes[0]];
 
-				$rev 		= array_reverse ($inter_nodes);
-				$buyer_node = &$nodes[indexTo ($rev[0], $nodes)];
+				$products = $buyer_node['requests'];
+				$products = unserialize_requests($products);
 
-				unset ($rev);
-				$products = $buyer_node['needs_product'];
-				$products = unserialize($products);
-				foreach($products as $product) {
-				//	check if buyer still needs to purchase products
-					if (check_buyer_needs_product ($product)) {
-						// finalize transaction
-						complete_purchase ($inter_nodes, $buyer_node, $seller_node, $nodes, $con);
+				foreach ($products as $product) {
+					if($product[0] == $seller_node['serves']) {
+						//	check if buyer still needs to purchase products
+						if (check_buyer_needs_product ($product)) {
+							// finalize transaction
+							complete_purchase ($inter_nodes, $buyer_node, $seller_node, $con);
+							$nodes[$seller_node["id"]] = $seller_node;
+							$nodes[$buyer_node["id"]] = $buyer_node;
+						}
 					}
 				}
 			}
 		}
-
 		//	update database with new values for money and product quantities
 		update_post_tranzaction ($con, $nodes);
 	}
-
 
 	//	refactorization of code to make code easier to manipulate
 	//	functions to add : 	check_seller_has_product (); check_buyer_affords_product (); get_final_purchase_amount (); get_final_purchase_cost_ppc ();
@@ -80,25 +78,24 @@
 	//===============================================================R=E=F=A=C=T=O=R=I=Z=A=T=I=ON===================================================\\
 	function check_seller_has_product ($seller_node){
 		//	returns TRUE if seller has a positive amount of product to sell (least amount is 0 -> returns FALSE)
-		if ($seller_node ['has_product_count'] > 0) return TRUE;
-
-		return FALSE;
+		return ($seller_node['quantity'] > 0);
 	}
 
 	function check_buyer_needs_product ($product){
 		//	returns TRUE if buyer still needs a quantity of product to buy (least amount is 0 -> returns FALSE)
-		if ($product ['p_count'] > 0) return TRUE;
-
-		return FALSE;
+		return ($product [1] > 0);
 	}
 
 	function get_initial_cost_ppc ($product, $con){
+		global $products;
 		$products = checkProductsGlobalVariable ($con);
 		
-		//	returns the value (float) of ONE instance of the product the buyer needs
-		$product = $products [(int) (substr ($product['p_name'], 1))];
-
-		return $product['value'];
+		foreach ($products as $temp) {
+			if($temp["name"] == $product[0]) {
+			//	returns the value (float) of ONE instance of the product the buyer needs
+				return $temp["base_cost"];
+			}
+		}
 	}
 
 	function get_final_cost_ppc ($inter_nodes, $product, $seller_node, $initial_cost_ppc){
@@ -106,9 +103,8 @@
 		$price = $initial_cost_ppc;
 
 		foreach ($inter_nodes as $intermediary) {
-			if ($intermediary !== $seller_node['name'] && $intermediary !== $product['p_name']) {
+			if ($intermediary !== $seller_node['id'] && $intermediary !== $product[0])
 				$price = calculateNewPrice ($price);
-			}
 		}
 
 		return $price;
@@ -118,15 +114,13 @@
 		//	returns the amount (int) of product the buyer can afford to buy from seller at the final per-piece cost
 		$affordable_amount = (int) ($buyer_node ['money'] / $final_cost_ppc);
 
-		if ($affordable_amount > $product ['p_count']){
-			$affordable_amount = $product ['p_count'];
-		}
+		if ($affordable_amount > $product [1])
+			$affordable_amount = $product [1];
 
-		if ($affordable_amount <= $seller_node ['has_product_count']){
+		if ($affordable_amount <= $seller_node ['quantity'])
 			return $affordable_amount;
-		}
 
-		return $seller_node ['has_product_count'];
+		return $seller_node ['quantity'];
 	}
 
 	function get_final_cost_whole ($final_purchase_amount, $final_cost_ppc){
@@ -134,31 +128,41 @@
 		return $final_purchase_amount * $final_cost_ppc;
 	}
 
-	function complete_purchase (&$inter_nodes, &$buyer_node, &$seller_node, &$nodes, $con){
+	function complete_purchase (&$inter_nodes, &$buyer_node, &$seller_node, $con){
+		global $nodes;
+
 		//	complete tranzaction ; after tranzaction is completed, each intermediary node involved in the transaction receives its share of the total profit
-		$products = $buyer_node['needs_product'];
-		$products = unserialize($products);
-		foreach ($products as $product) {
+		$products = $buyer_node['requests'];
+		$products = unserialize_requests($products);
+
+		foreach ($products as &$product) {
 			$initial_cost_ppc 		= get_initial_cost_ppc ($product, $con);
 			$final_cost_ppc 		= get_final_cost_ppc ($inter_nodes, $product, $seller_node, $initial_cost_ppc);
 			$final_purchase_amount 	= get_final_purchase_amount ($buyer_node, $product, $seller_node, $final_cost_ppc);
 			$final_cost_whole 		= get_final_cost_whole ($final_purchase_amount, $final_cost_ppc);
+			$result 				= ''; 
 
-			$product ['p_count']	-= $final_purchase_amount;
-			$buyer_node ['money']				-= $final_cost_whole;
+			$product [1]			-= $final_purchase_amount;
+			$buyer_node ['money'] 	-= $final_cost_whole;
+
+			for ($i=0; $i < sizeof($products); $i++) { 
+				$result .= implode('|', $products[$i])."^";
+			}
+
+			$buyer_node ['requests'] = substr($result, 0, -1);
 
 			foreach (array_reverse($inter_nodes) as $intermediary){
 				// each intermediary node involved in the transaction will receive 10% of the final cost
-				if ($intermediary !== $seller_node['name'] && $intermediary !== $product['p_name']) {
-					$inter_node 			= &$nodes[indexTo ($intermediary, $nodes)];
+				if ($intermediary !== $seller_node['id'] && $intermediary !== $product[0]) {
+					$inter_node 			= $nodes[$intermediary];
 					$interm_profit 			= ($final_cost_whole / 11);
 					$inter_node['money']	+= $interm_profit;
 					$final_cost_whole 		-= $interm_profit;
 				}
 			}
 
-			$seller_node ['has_product_count']	-= $final_purchase_amount;
-			$seller_node ['money']				+= $final_cost_whole;
+			$seller_node ['quantity']   -= $final_purchase_amount;
+			$seller_node ['money']		+= $final_cost_whole;
 		}
 	}
 	//===========================================================================E=N=D==============================================================\\
@@ -171,7 +175,7 @@
 	//	update database with new values of product counts and moneys
 	function update_post_tranzaction ($con, $nodes){
 		foreach($nodes as $nd){
-			execute_sql_and_return('<simulator.php>', $con, "UPDATE nodes SET has_product_count = ".$nd['has_product_count'].", money = ".$nd['money']." WHERE name = '" . $nd['name']. "'");
+			execute_sql_and_return('<simulator.php>', $con, "UPDATE nodes SET quantity = ".$nd['quantity'].", money = ".$nd['money']." WHERE id = '" . $nd['id']. "'");
 		}
 	}
 
@@ -179,7 +183,6 @@
 	function calculateNewPrice ($old_price) {
 		// constant profit expressed in percentage (%) of $old_price
 		$personal_profit = 10;
-
 		return $old_price + (($personal_profit / 100) * $old_price);
 	}
 
@@ -268,9 +271,8 @@
 		global $Q;
 
 		//	if $end's parent is itself, return $end ; this means we've reached the source node as it is the only one with this property
-		if ($Q[$end] === $start) {
+		if ($Q[$end] === $start)
 			return $end;
-		}
 
 		//	get $end's parent from the heap
 		$p_node = $Q[$end];
@@ -300,9 +302,8 @@
 
 		$rez = '';
 		for ($i = 0; $i < sizeof($nodes); $i++){
-			if ($nodes[$i]['serves'] === $product_name){
+			if ($nodes[$i]['serves'] === $product_name)
 				$rez .= $i . ',';
-			}
 		}
 
 		$rez = trim($rez, ',');
@@ -326,18 +327,16 @@
 		foreach ($requests as &$request) {
 			$request = explode('|', $request);
 
-			if ($request[2] > $max_priority) {
+			if ($request[2] > $max_priority)
 				$max_priority = $request[2];
-			}
 		}
 
 		$rez = [];
 
 		while ($max_priority >= 0) {
 			foreach ($requests as $request) {
-				if ($request[2] === $max_priority) {
+				if ($request[2] === $max_priority)
 					$rez = array_merge ($rez, getProvidersOf($request[0]));
-				}
 			}
 
 			$max_priority--;
