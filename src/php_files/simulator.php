6296b7fef8dc60a7969c12bbe5f307d8d72b6ad9
Alexandru Stana <alexandrustana2@gmail.com>
2016-03-26 11:53:57 +0200

    added time counter feature

--- src/php_files/simulator.php
+++ src/php_files/simulator.php
@@ -19,9 +19,21 @@
 	if ($con->connect_error) {
 	    die ("Connection failed: " . $conn->connect_error);
 	}
+	
+	//place this before any script you want to calculate time
+$time_start = microtime(true); 
+
 
 	// start simulator by setting the execution number
-	prepareSim (10, $con);
+	prepareSim (100, $con);
+	
+	$time_end = microtime(true);
+
+//dividing with 60 will give the execution time in minutes other wise seconds
+$execution_time = ($time_end - $time_start)/60;
+
+//execution time of the script
+echo '<b>Total Execution Time:</b> '.$execution_time.' Mins';
 
 	// close connection
 	$con->close ();
--- src/php_files/sql_execute.php
+++ src/php_files/sql_execute.php
@@ -1,4 +1,5 @@
 <?php	
+	ini_set('max_execution_time', 300); //300 seconds = 5 minutes
 	// execution of sql command and error handling
 	function execute_sql ($file, $con, $sql) {
 		if ($con->query($sql) !== TRUE) {
@@ -8,5 +9,5 @@
 
 	// execution of sql command return value
 	function execute_sql_and_return ($file, $con, $sql) {
-		return mysqli_query($con, $sql);
+			return mysqli_query($con, $sql);
 	}
